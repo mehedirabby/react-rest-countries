4755b64df282f70a4676b8ff460f591af714662a
Mehedi Rabby <mehedi15-2995@diu.edu.bd>
2022-09-23 21:53:34 +0600

    anothhhhher

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
 
 import Countries from './Components/Countries/Countries';
+import Footer from './Components/Footer/Footer';
+import Header from './Components/Header/Header';
 
 
 
 function App() {
   return (
     <div className="App">
-      
+
+
+        <Header></Header>
         <Countries></Countries> 
+        <Footer></Footer>
         
 
       
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <p>Copy Right Protected</p>
+        </div>
+    );
+};
+
+export default Footer;
